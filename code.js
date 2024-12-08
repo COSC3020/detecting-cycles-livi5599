3e432e1c0fe3824dd8949d83def6139bf399459a
livi5599 <obrown1@uwyo.edu>
2024-12-07 23:14:26 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,25 +1,26 @@
 function hasCycle(graph) {
     let queue = [];
     let visited = [];
-    let current = graph[0];
+    let current = 0;
     let parent = null;
     visited.push(current);
     queue.push(current);
+
     while (queue.length != 0) {
         current = queue.shift();
-        for (let neighbor = 0; neighbor < current.length; neighbor++) {
-            if (current[neighbor] == 0) {
+        for (let neighbor = 0; neighbor < graph[current].length; neighbor++) {
+            if (graph[current][neighbor] === 0) {
                 continue;
             }
-            else if (graph[current[neighbor]] == parent) {
+            else if (neighbor === parent) {
                 continue;
             }
-            else if (visited.includes(graph[current[neighbor]])) {
+            else if (visited.includes(neighbor)) {
                 return true;
             }
             else {
-                visited.push(graph[current[neighbor]]);
-                queue.push(graph[current[neighbor]]);
+                visited.push(neighbor);
+                queue.push(neighbor);
             }
         }
         parent = current;
